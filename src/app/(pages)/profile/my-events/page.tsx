0c42215d2370e2f3117b2ad740914fd60df80935
mangareira <ghabryellsantos81@gmail.com>
2024-04-30 15:02:40 -0300

    modifyer-pagination

--- src/app/(pages)/profile/my-events/page.tsx
+++ src/app/(pages)/profile/my-events/page.tsx
@@ -3,6 +3,7 @@ import { CardEvent } from "@/app/components/Form/CardEvent"
 import { Pagination } from "@/app/components/Pagination"
 import { FetchWrapper } from "@/app/utils/FetchWrapper"
 import { ReactNode, useEffect, useState } from "react"
+import { AiOutlineLoading } from "react-icons/ai"
 type PageProps = {
     searchParams?: {page?: string; limit?: string}
 }
@@ -23,24 +24,26 @@ export default function myEvents ({searchParams}: PageProps) {
     }, [])    
     const p = () => {
         if(!quantity) {
-            return <div className="">Carregando...</div>
+            return <div className="absolute top-[50%] left-[45%]"><AiOutlineLoading className="animate-spin text-blue" size={30}/></div>
         }
-        return <Pagination limit={limit} page={page} total={quantity} classNameDiv="my-8" />
+        return <Pagination limit={limit} page={page} total={quantity} classNameDiv="flex justify-center" />
     }
     return (
-        <div className="container mx-auto">
-            <div className="text-blue py-2">
-                <div className="text-2xl font-medium">Meus Eventos</div>
-                <div className=" text-base font-light ">Aqui podera ver todos seus eventos</div>
-            </div>
-            <div className="grid grid-cols-2 gap-4">
-                {events.map((e: any, index: any) => (
-                    <div className="" key={index}>
-                        <CardEvent events={e}/>
+        <div className="container mx-auto h-[85vh]">
+            <div className="h-full">
+                <div className="text-blue py-2">
+                    <div className="text-2xl font-medium">Meus Eventos</div>
+                    <div className=" text-base font-light ">Aqui podera ver todos seus eventos</div>
+                </div>
+                <div className="h-[83%]">
+                    <div className="grid grid-cols-2 gap-4">
+                        {events.map((e: any, index: any) => (
+                            <CardEvent events={e} key={index}/>
+                        ))}
                     </div>
-                ))}
+                </div>
+                {p()}
             </div>
-            <div className="">{p()}</div>
         </div>
     )
 }
--- src/app/components/Pagination/index.tsx
+++ src/app/components/Pagination/index.tsx
@@ -1,35 +1,36 @@
 import { IPagination } from "@/app/utils/interface";
 import { L, R, usePagination } from "@/app/utils/usePagination";
 import Link from "next/link";
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export const Pagination = ({page, limit, total, classNameDiv}: IPagination) => {
     const pathName = usePathname()
     const seachParams = useSearchParams()
+    const router = useRouter()
     const {pages, isCurrentPage} =  usePagination({page,limit,total})   
-    const reloadPage = async () => {
-        await new Promise(resolve => setTimeout(resolve, 200))
-        window.location.reload()
-    }
     const generateUrl = (page: number) => {
         const params = new URLSearchParams(seachParams)
         params.set("page", page.toString())
         const url = `${pathName}?${params.toString()}`
         return url
     }
+    const query = async () => {
+        await new Promise(resolve => setTimeout(resolve, 700))
+        location.reload()
+    }
     return(
         <div className={classNameDiv}>
-            <ul className="inline-flex -space-x-px text-base h-10" >
+            <ul className="inline-flex -space-x-px text-base h-10 " >
                 {pages.map((page: any, index) => {
                     const isEllipsis = page === L || page === R
                     const isFirst = index === 0
                     const isLast = index + 1 === pages.length
                     const isCurrent = isCurrentPage(page)
-                    const className = ['flex items-center justify-center px-4 h-10 leading-tight border-gray-300', 
-                    isFirst ? 'ms-0 border-e-0 rounded-s-lg' : '',
+                    const className = ['flex items-center justify-center px-4 h-10 leading-tight ', 
+                    isFirst ? 'ms-0 rounded-s-lg' : '',
                     isLast ? 'rounded-e-lg': '',
-                    isCurrent ? "text-blue-600 bg-blue-50 hover:bg-blue-100 dark:bg-gray-700 dark:border-gray-600 darl:bg-gray-50" :
-                    "text-gray-500 bg-white hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-500" ].join(' ')
+                    isCurrent ? "text-blue bg-gray-300 hover:bg-blue-600 border-blue border hover:text-white" :
+                    "text-blue bg-white hover:bg-gray-100 border-blue border" ].join(' ')
                     if(isEllipsis) {
                         return (
                             <li key={index} >
@@ -39,8 +40,7 @@ export const Pagination = ({page, limit, total, classNameDiv}: IPagination) => {
                             </li>)
                     }
                     return <li key={index} >
-                        <Link href={generateUrl(page)} onClick={reloadPage} 
-                        className={className}>
+                        <Link href={generateUrl(page)} className={className} onClick={query}>
                             {page}
                         </Link>
                     </li>
